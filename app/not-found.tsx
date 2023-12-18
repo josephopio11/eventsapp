10f8e8f61aaa29014496345d2cad1c0dc6f05b19
Joseph Opio <josephopio11@gmail.com>
2023-12-18 13:52:24 +0800

    Added a 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,6 @@
 import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
+import NotFoundLinks from "@/components/shared/NotFoundLinks";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -10,15 +9,7 @@ export default function NotFound() {
   return (
     <>
       <Header />
-      <main className="flex min-h-[70vh] flex-col items-center justify-center">
-        <div className="flex flex-row ">
-          <h2 className="text-5xl font-bold">404</h2>
-          <div className="border-r px-2 mr-2" />
-          <p className="">
-            The page you are trying <br /> to access ca not be found
-          </p>
-        </div>
-      </main>
+      <NotFoundLinks />
       <Footer />
     </>
   );
--- components/shared/NotFoundLinks.tsx
+++ components/shared/NotFoundLinks.tsx
@@ -1,17 +1,29 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
+
 const NotFoundLinks = () => {
+  const router = useRouter();
+
+  const goBack = () => {
+    router.back();
+  };
   return (
-    <div>
-      <Link href="/" className="w-36">
-        <Image
-          src="/assets/images/logo.svg"
-          width={128}
-          height={38}
-          alt="Evently logo"
-        />
-      </Link>
-    </div>
+    <main className="flex min-h-[70vh] flex-col items-center justify-center">
+      <div className="flex flex-row ">
+        <h2 className="text-5xl font-bold">404</h2>
+        <div className="border-r px-2 mr-2" />
+        <p className="">
+          The page you are trying <br /> to access ca not be found
+        </p>
+      </div>
+      <div className="flex flex-row mt-10">
+        <Button onClick={goBack}>Go Back</Button>
+      </div>
+    </main>
   );
 };
 
